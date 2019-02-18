114c0fc1f1e333ebac3ecc19a4142f97b861628d
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-02-18 23:05:10 +0800

    Not working

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -3,27 +3,64 @@ pub mod literals;
 mod utils;
 
 pub use errors::Error;
-use nom::{alt, named, ws};
+use nom::{alt_complete, call, char, named, separated_pair, ws};
 
 use std::collections::HashMap;
 
+pub type Map<'a> = HashMap<literals::Key<'a>, Value<'a>>;
+
 #[derive(Debug, PartialEq)]
 /// Value in HCL
-pub enum Value {
+pub enum Value<'a> {
     Integer(i64),
     Float(f64),
     Boolean(bool),
     String(String),
-    Array(Vec<Value>),
-    Stanza(Stanza),
-    Object(HashMap<String, Value>),
+    Array(Vec<Value<'a>>),
+    Stanza(Stanza<'a>),
+    Map(Map<'a>),
 }
 
 // https://github.com/Geal/nom/blob/master/tests/json.rs
 #[derive(Debug, PartialEq)]
-pub struct Stanza {
+pub struct Stanza<'a> {
     pub keys: Vec<String>,
-    pub values: HashMap<String, Value>,
+    pub values: Map<'a>,
 }
 
-// Parse values of the form "key" = "..." | ["..."] | {...}
+/// Parse values of the form "key" = "..." | ["..."] | {...}
+named!(
+    pub key_value(&str) -> (literals::Key, Value),
+    ws!(
+        separated_pair!(
+            literals::key,
+            char!('='),
+            alt_complete!(
+                call!(crate::utils::wrap_str(literals::integer)) => { |v| Value::Integer(v) }
+                | call!(crate::utils::wrap_str(literals::float)) => { |v| Value::Float(v) }
+            )
+        )
+    )
+);
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use utils::ResultUtilsString;
+
+    #[test]
+    fn key_value_pairs_are_parsed_successfully() {
+        let test_cases = [
+            (r#"test = 123"#, ("test", Value::Integer(123))),
+        ];
+
+        for (input, (expected_key, expected_value)) in test_cases.into_iter() {
+            println!("Testing {}", input);
+            let (actual_key, actual_value) = key_value(input).unwrap_output();
+            assert_eq!(actual_key.unwrap(), *expected_key);
+            assert_eq!(actual_value, *expected_value);
+        }
+    }
+}
--- lib/src/literals.rs
+++ lib/src/literals.rs
@@ -2,11 +2,16 @@
 
 pub mod strings;
 
+pub use strings::string;
+
+use std::borrow::Cow;
+use std::ops::Deref;
 use std::str::FromStr;
 
 use nom::types::CompleteStr;
 use nom::{
-    alt, call, complete, do_parse, map, map_res, named, one_of, opt, recognize, tag, verify,
+    alt, alt_complete, call, complete, do_parse, map, map_res, named, one_of, opt, recognize, tag,
+    verify,
 };
 
 /// Parsed Integer Literal
@@ -70,6 +75,42 @@ named!(pub identifier(CompleteStr) -> &str,
     )
 );
 
+/// A "key" in a map
+#[derive(Eq, PartialEq, Debug, Hash, Clone)]
+pub enum Key<'a> {
+    Identifier(Cow<'a, str>),
+    String(Cow<'a, str>),
+}
+
+impl<'a> Key<'a> {
+    pub fn unwrap(self) -> Cow<'a, str> {
+        match self {
+            Key::Identifier(s) => s,
+            Key::String(s) => s,
+        }
+    }
+}
+
+impl<'a> Deref for Key<'a> {
+    type Target = str;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Key::Identifier(s) => s.deref(),
+            Key::String(s) => s.deref(),
+        }
+    }
+}
+
+/// Parse a "key" for a map
+named!(
+    pub key(&str) -> Key,
+    alt_complete!(
+        call!(crate::utils::wrap_str(identifier)) => { |s| Key::Identifier(Cow::Borrowed(s)) }
+        | string => { |s| Key::String(Cow::Owned(s)) }
+    )
+);
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -117,6 +158,22 @@ mod tests {
         for input in test_cases.into_iter() {
             println!("Testing {}", input);
             assert!(identifier(CompleteStr(input)).is_err());
+        }
+    }
+
+    #[test]
+    fn keys_are_parsed_correctly() {
+        let test_cases = [
+            ("abcd123", Key::Identifier(From::from("abcd123"))),
+            ("_abc", Key::Identifier(From::from("_abc"))),
+            ("藏_①", Key::Identifier(From::from("藏_①"))),
+            (r#""123""#, Key::String(From::from("123"))),
+            (r#""a/b/c""#, Key::String(From::from("a/b/c"))),
+        ];
+
+        for (input, expected) in test_cases.into_iter() {
+            println!("Testing {}", input);
+            assert_eq!(key(input).unwrap_output(), *expected);
         }
     }
 }
