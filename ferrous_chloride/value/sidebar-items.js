61e4fd7ee77bdec7f2b2478ce95c7404c46d6851
Yong Wen Chua <deploy@travis-ci.org>
2019-03-25 14:38:46 +0000

    Deploy lawliet89/ferrous-chloride to github.com/lawliet89/ferrous-chloride.git:gh-pages

--- ferrous_chloride/value/sidebar-items.js
+++ ferrous_chloride/value/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Value","Value in HCL"]],"fn":[["key_value",""],["list",""],["map_values",""],["single_value",""]],"type":[["Block",""],["List",""],["Map",""],["MapValues",""]]});
+initSidebarItems({"enum":[["Value","Value in HCL"]],"fn":[["key_value",""],["list",""],["map_values",""],["single_value",""]],"type":[["Block","Contains a list of HCL Blocks sharing the same identifier with one or more labels differentiating each Block from each other."],["List",""],["Map",""],["MapValues",""]]});
