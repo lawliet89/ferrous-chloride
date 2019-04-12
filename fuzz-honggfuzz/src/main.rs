bb9c20ea34cc5311dcff1321c695f722e5a29ae7
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-04-12 20:54:32 +0800

    Don't build honggfuzz on Windows

--- fuzz-honggfuzz/src/main.rs
+++ fuzz-honggfuzz/src/main.rs
@@ -1,15 +1,36 @@
-#[macro_use]
-extern crate honggfuzz;
+#[cfg(not(target_os = "windows"))]
+mod non_windows {
+    use ferrous_chloride::parse_str;
+    use honggfuzz::fuzz;
 
-use ferrous_chloride::parse_str;
-
-#[rustfmt::skip]
-fn main() {
-    loop {
-        fuzz!(|data: &[u8]| {
-            if let Ok(s) = std::str::from_utf8(data) {
-                let _ = parse_str(s);
-            }
-        });
+    #[rustfmt::skip]
+    pub fn main() {
+        loop {
+            fuzz!(|data: &[u8]| {
+                if let Ok(s) = std::str::from_utf8(data) {
+                    let _ = parse_str(s);
+                }
+            });
+        }
     }
+}
+
+#[cfg(target_os = "windows")]
+mod windows {
+    pub fn main() {
+        panic!(
+            "honggfuzz-rs does not currently support Windows but \
+             works well under WSL (Windows Subsystem for Linux)"
+        );
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+use non_windows as implementation;
+
+#[cfg(target_os = "windows")]
+use windows as implementation;
+
+fn main() {
+    implementation::main();
 }
