b5031f5d299bf71e3ac66a38538c422e22213927
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-04-12 21:04:00 +0800

    Fix fuzzer compile errors on Windows

--- fuzz-afl/src/main.rs
+++ fuzz-afl/src/main.rs
@@ -1,15 +1,36 @@
-#[macro_use]
-extern crate afl;
+#[cfg(not(target_os = "windows"))]
+mod non_windows {
+    use afl::fuzz;
+    use ferrous_chloride::parse_str;
 
-use ferrous_chloride::parse_str;
-
-#[rustfmt::skip]
-fn main() {
-    loop {
-        fuzz!(|data: &[u8]| {
-            if let Ok(s) = std::str::from_utf8(data) {
-                let _ = parse_str(&s);
-            }
-        });
+    #[rustfmt::skip]
+    pub fn main() {
+        loop {
+            fuzz!(|data: &[u8]| {
+                if let Ok(s) = std::str::from_utf8(data) {
+                    let _ = parse_str(&s);
+                }
+            });
+        }
     }
+}
+
+#[cfg(target_os = "windows")]
+mod windows {
+    pub fn main() {
+        panic!(
+            "afl does not build in Windows but \
+             works well under WSL (Windows Subsystem for Linux)"
+        );
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+use non_windows as implementation;
+
+#[cfg(target_os = "windows")]
+use windows as implementation;
+
+fn main() {
+    implementation::main();
 }
