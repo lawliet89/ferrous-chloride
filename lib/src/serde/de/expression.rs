0accc7db1e387724459e9ce31e272e1d79c6895e
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-04-14 16:46:25 +0800

    Change bytes

--- lib/src/serde/de/expression.rs
+++ lib/src/serde/de/expression.rs
@@ -1,8 +1,8 @@
-use serde::de::{self, Visitor};
+use serde::de::{self, IntoDeserializer, Visitor};
 use serde::forward_to_deserialize_any;
 
 use crate::parser::expression::Expression;
-use crate::serde::de::{deserialize_number, deserialize_string, Compat};
+use crate::serde::de::{deserialize_number, deserialize_string, deserialize_tuple, Compat};
 
 impl<'de> de::Deserializer<'de> for Expression<'de> {
     type Error = Compat;
@@ -17,7 +17,7 @@ impl<'de> de::Deserializer<'de> for Expression<'de> {
             Number(number) => deserialize_number(number, visitor),
             Boolean(boolean) => visitor.visit_bool(boolean),
             String(string) => deserialize_string(string, visitor),
-            Tuple(_tuple) => unimplemented!("Not yet"),
+            Tuple(tuple) => deserialize_tuple(tuple, visitor),
             Object(_object) => unimplemented!("Not yet"),
         }
     }
@@ -29,10 +29,116 @@ impl<'de> de::Deserializer<'de> for Expression<'de> {
     }
 }
 
+impl<'de> IntoDeserializer<'de, Compat> for Expression<'de> {
+    type Deserializer = Self;
+
+    fn into_deserializer(self) -> Self::Deserializer {
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use serde::Deserialize;
+    use std::borrow::Cow;
 
     #[test]
-    fn test_parsing() {}
+    fn deserialize_boolean() {
+        let expression = Expression::from(true);
+        let deserialized = bool::deserialize(expression).unwrap();
+        assert_eq!(deserialized, true);
+
+        let expression = Expression::from(false);
+        let deserialized = bool::deserialize(expression).unwrap();
+        assert_eq!(deserialized, false);
+    }
+
+    #[test]
+    fn deserialize_integer() {
+        let expression = Expression::from(12345);
+        let deserialized = u32::deserialize(expression).unwrap();
+        assert_eq!(deserialized, 12345);
+
+        let expression = Expression::from(-12345);
+        let deserialized = i32::deserialize(expression).unwrap();
+        assert_eq!(deserialized, -12345);
+    }
+
+    #[test]
+    #[should_panic(expected = "expected u8")]
+    fn deserialize_integer_checks_for_overflow() {
+        let expression = Expression::from(12345);
+        let _ = u8::deserialize(expression).unwrap();
+    }
+
+    #[test]
+    #[allow(clippy::float_cmp)] // Don't be a pedant!
+    fn deserialize_float() {
+        let expression = Expression::from(12345);
+        let deserialized = f64::deserialize(expression).unwrap();
+        assert_eq!(deserialized, 12345.);
+
+        let expression = Expression::from(-12345.12);
+        let deserialized = f32::deserialize(expression).unwrap();
+        assert_eq!(deserialized, -12345.12);
+    }
+
+    #[test]
+    fn deserialize_string() {
+        let expression = Expression::String(Cow::Owned("foobar".to_string()));
+        let deserialized = String::deserialize(expression).unwrap();
+        assert_eq!(deserialized, "foobar");
+    }
+
+    #[test]
+    fn deserialize_borrowed_string() {
+        let string = "foobar".to_string();
+        let expression = Expression::String(Cow::Borrowed(&string));
+        let deserialized: &str = Deserialize::deserialize(expression).unwrap();
+        assert_eq!(deserialized, string);
+    }
+
+    #[test]
+    #[should_panic(expected = "expected a borrowed string")]
+    fn deserialize_borrowed_string_errors_when_invalid() {
+        let deserializer = Expression::String(Cow::Owned("input".to_string()));
+        let _: &str = Deserialize::deserialize(deserializer).unwrap();
+    }
+
+    #[test]
+    fn deserialize_char() {
+        let deserializer = Expression::from("c");
+        let deserialized = char::deserialize(deserializer).unwrap();
+        assert_eq!(deserialized, 'c');
+    }
+
+    #[test]
+    #[should_panic(expected = "expected a character")]
+    fn deserialize_char_should_error_on_strings() {
+        let deserializer = Expression::from("this will fail");
+        let _ = char::deserialize(deserializer).unwrap();
+    }
+
+    #[test]
+    fn deserialize_bytes() {
+        use serde_bytes::ByteBuf;
+
+        let string = b"hello world";
+        let deserializer =
+            Expression::Tuple(string.to_vec().into_iter().map(Expression::from).collect());
+        let deserialized = ByteBuf::deserialize(deserializer).unwrap();
+
+        let actual: &[u8] = deserialized.as_ref();
+        assert_eq!(actual, string);
+    }
+
+    #[test]
+    #[should_panic(expected = "expected u8")]
+    fn deserialize_bytes_errors_on_invalid_entries() {
+        use serde_bytes::ByteBuf;
+
+        let deserializer = Expression::Tuple(vec![Expression::from(false), Expression::from("hi")]);
+        let _ = ByteBuf::deserialize(deserializer).unwrap();
+    }
 }
